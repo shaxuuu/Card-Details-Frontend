bff9a255289c6205c64fa2669e25d9c58b61eac7
100206 <xvenumpl@gmail.com>
2023-10-02 18:11:11 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { CardDetails } from './components/cardDetails/CardDetails'
+
+function App() {
+
+  return (
+    <>
+      <CardDetails></CardDetails>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/cardDetails/CardDetails.jsx
@@ -0,0 +1,15 @@
+import desktopBackground from '../../assets/backgrounds/bg-main-desktop.png'
+import mobileBackground from '../../assets/backgrounds/bg-main-mobile.png'
+
+export const CardDetails = () => {
+
+
+    return (
+        <>
+        
+            <div> </div>
+            <div> </div>
+
+        </>
+    )
+}
