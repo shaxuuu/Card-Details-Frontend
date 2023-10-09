03c673a7fe540bfc66f062421d1abf09765aee45
shxu <48029949+XvenumPL@users.noreply.github.com>
2023-10-09 03:00:12 +0200

    formsFinished

--- src/components/cardDetails/CardDetails.jsx
+++ src/components/cardDetails/CardDetails.jsx
@@ -6,10 +6,12 @@ import { compileString } from "sass"
 
 export const CardDetails = () => {
 
-    const inputRefs = { name : useRef(), card : useRef() }
+    const inputRefs = { name : useRef(), card : useRef(), month : useRef(), year : useRef(), cvc : useRef() }
     const [ nameState, changeName ] = useState("")
     const [ ccNumberState, changeCcNumber ] = useState("")
     const [ cvcState, changeCvc ] = useState("")
+    const [ monthState, changeMonth ] = useState(undefined)
+    const [ yearState, changeYear ] = useState(undefined)
 
     const checkIfNumeric = ( input ) => {
         if( !( input.keyCode >= 48 && input.keyCode <= 57 ) && input.key !== 'Backspace' && input.key !== 'ArrowLeft'  && input.key !== 'ArrowRight' ){
@@ -67,49 +69,119 @@ export const CardDetails = () => {
     }
 
     const inputCheckMonth = ( input ) => {
+        if( !checkIfNumeric( input ) ){
+            input.preventDefault()
+        }else if( input.key !== 'Backspace'){
+
+            if( Number( input.key ) >= 2 && inputRefs.month.current.value.length <= 1 && inputRefs.month.current.value.charAt(0) !== '1' ){
+                inputRefs.month.current.value = '0' + input.key
+                changeMonth( '0' + input.key )
+                inputRefs.year.current.focus()
+                input.preventDefault()
+
+            }else if( inputRefs.month.current.value.charAt(0) === '1' ) {
+                if( Number(input.key) > 2 ){
+                    inputRefs.month.current.value = '01'
+                    changeMonth( '01' )
+                }else{
+                    inputRefs.month.current.value = '1' + input.key
+                    changeMonth( '1' + input.key )
+                }
+                inputRefs.year.current.focus()
+                input.preventDefault()
+
+            }else if( inputRefs.month.current.value.length === 2 ){
+                inputRefs.year.current.focus()
+                input.preventDefault()
+            }
+
+        }
 
     }
 
+    const inputMonthLostFocus = () => {
+        if( inputRefs.month.current.value === '1' ){
+            inputRefs.month.current.value = '01'
+            changeMonth('01')
+        }
+    }
+
+    const inputCheckYear = ( input ) => {
+        if( !checkIfNumeric( input ) ){
+            input.preventDefault()
+        }else{
+
+        }
+
+    }
+
+    const validateForm = () => {
+        if( inputRefs.name.current.value.length === 0 ){
+            inputRefs.name.current.classList.add('invalidInput')
+        }else{
+            inputRefs.name.current.classList.remove('invalidInput')
+        }
+        if( inputRefs.card.current.value.length !== 19 ){
+            inputRefs.card.current.classList.add('invalidInput')
+        }else{
+            inputRefs.card.current.classList.remove('invalidInput')
+        }
+        if( inputRefs.month.current.value.length !== 2 ){
+            inputRefs.month.current.classList.add('invalidInput')
+        }else{
+            inputRefs.month.current.classList.remove('invalidInput')
+        }
+        if( inputRefs.year.current.value.length !== 2 ){
+            inputRefs.year.current.classList.add('invalidInput')
+        }else{
+            inputRefs.year.current.classList.remove('invalidInput')
+        }
+        if( inputRefs.cvc.current.value.length < 3 ){
+            inputRefs.cvc.current.classList.add('invalidInput')
+        }else{
+            inputRefs.cvc.current.classList.remove('invalidInput')
+        }
+    }
 
 
     return (
         <div id="cardDetailsContent">
 
             <div id="cardDetailsCards">
-                <CardFront data={{ name : nameState, ccNumber : ccNumberState }}/>
+                <CardFront data={{ name : nameState, ccNumber : ccNumberState, month : monthState, year : yearState }}/>
                 <CardBack data={{ cvc : cvcState }} />
             </div>
 
-            <div id="cardDetails">
+            <form id="cardDetails">
             
                 <div className="inputHolder">
                     <span className="fieldTitle">Cardholder Name</span>
-                    <input className="detailsInput" placeholder="e.g. Jane Appleseed" ref={inputRefs.name} onChange={() => ( changeName( inputRefs.name.current.value ) )} pattern="[A-Za-z]*" onKeyDown={(event) => {inputCheckStrgin(event)}} type="text" id="name" /> 
+                    <input className="detailsInput" placeholder="e.g. Jane Appleseed" ref={inputRefs.name} onChange={() => ( changeName( inputRefs.name.current.value ) )} onKeyDown={(event) => {inputCheckStrgin(event)}} type="text" id="name" required/> 
                 </div>
                 
                 <div className="inputHolder">
                     <span className="fieldTitle">Card Number</span>
-                    <input className="detailsInput" placeholder="e.g. 1234 5678 9123 0000" ref={inputRefs.card} onChange={() => ( changeCcNumber( inputRefs.card.current.value ) )} onKeyDown={(event) => {inputCheckCard(event)}} maxLength="19" type="text" id="cardNumber" />
+                    <input className="detailsInput" placeholder="e.g. 1234 5678 9123 0000" ref={inputRefs.card} onChange={() => ( changeCcNumber( inputRefs.card.current.value ) )} onKeyDown={(event) => {inputCheckCard(event)}} maxLength="19" type="text" id="cardNumber" required/>
                 </div>
                 
                 <div className="inputHolder" data-direction="row">
                     <div>
                         <span className="fieldTitle">Exp. Date (MM/YY)</span>
                         <div className="cardDate">
-                            <input className="detailsInput" placeholder="MM" max="2" type="text" maxLength="2" id="Month" />
-                            <input className="detailsInput" placeholder="YY" type="text" maxLength="2" id="Year" />
+                            <input className="detailsInput" placeholder="MM" ref={inputRefs.month} type="text" maxLength="2" onBlur={inputMonthLostFocus} onKeyDown={inputCheckMonth} id="Month" required/>
+                            <input className="detailsInput" placeholder="YY" ref={inputRefs.year} type="text" maxLength="2" onChange={() => ( changeYear( inputRefs.year.current.value ) )} onKeyDown={inputCheckYear} id="Year" required/>
                         </div>
                     </div>
                     <div>
                         <span className="fieldTitle">CVC</span>
-                        <input className="detailsInput" placeholder="e.g. 123" type="text" onKeyDown={(event) => {inputCheckCvc(event)}} onChange={(event) => changeCvc(event.currentTarget.value)} maxLength="4" id="CVC" />
+                        <input className="detailsInput" placeholder="e.g. 123" ref={inputRefs.cvc} type="text" onKeyDown={(event) => {inputCheckCvc(event)}} onChange={() => changeCvc(inputRefs.cvc.current.value)} maxLength="4" id="CVC" required/>
                     </div>
 
                 </div>
 
-                <button className="cardDetailsSubmit">Confirm</button>
+                <button className="cardDetailsSubmit" onClick={validateForm}>Confirm</button>
 
-            </div>
+            </form>
 
         </div>
     )
--- src/components/cardDetails/CardFront.jsx
+++ src/components/cardDetails/CardFront.jsx
@@ -3,6 +3,7 @@ export const CardFront = ( props ) => {
     const name = props.data.name.split(" ") 
     let ccNumber = props.data.ccNumber.split(" ") 
     ccNumber = [ ccNumber[0] !== '' ?  ccNumber[0] : "0000", ccNumber[1] ?? "0000", ccNumber[2] ?? "0000", ccNumber[3] ?? "0000" ]
+    const date = { year : props.data.year ?? "00", month : props.data.month ?? "00" }
 
     return( 
         <div className="cardDetailsCard" data-side="front"> 
@@ -13,7 +14,7 @@ export const CardFront = ( props ) => {
             </div>
 
             <span className="cardNumber">   <span className="ccNumberGroup"> {ccNumber[0]} </span>   <span className="ccNumberGroup"> {ccNumber[1]} </span>   <span className="ccNumberGroup"> {ccNumber[2]} </span>   <span className="ccNumberGroup"> {ccNumber[3]} </span>   </span>
-            <span className="nameAndDate"> <span>{ name[0] != '' ? name[0] : "NAME" } { name[name.length - 1] != undefined && name.length > 1 ? name[name.length - 1] : "SURNAME" } </span>  <span>00/00</span> </span>
+            <span className="nameAndDate"> <span>{ name[0] != '' ? name[0] : "NAME" } { name[name.length - 1] != undefined && name.length > 1 ? name[name.length - 1] : "SURNAME" } </span>  <span>{date.month}/{date.year}</span> </span>
 
 
         </div>
